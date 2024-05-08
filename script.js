2ee97acbc61d8d32392777ed9f6f0c65836c0b82
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-05-08 11:46:30 +0545

    Make function getHumanChoice
    Made function to prompt user to choose between
    rock,paper and scissors

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ function getComputerChoice() {
     
     computerChoice = Math.random() * 3;
     if (computerChoice >= 0 && computerChoice < 1) {
-        return "Rock" + String(computerChoice);
+        return "Rock " + String(computerChoice);
     }
     else if (computerChoice >= 1 && computerChoice < 2) {
-        return "Paper" + String(computerChoice);
+        return "Paper " + String(computerChoice);
     }
 
     else {
-        return "Scissors" + String(computerChoice)
+        return "Scissors " + String(computerChoice)
     }
 }
 
 for (let i = 0; i < 15; i ++) {
     console.log(getComputerChoice());
 }
+
+// function to get human choice
+
+function getHumanChoice() {
+    humanChoice = prompt("Choose between Rock, Paper or Scissors");
+    return humanChoice;
+}
+console.log(getHumanChoice());
