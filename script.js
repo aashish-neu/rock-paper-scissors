cc89d6fd7bf94660ec2c38dc441500f3f8abdfc4
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-05-08 12:38:12 +0545

    Make playRound() function
    Made function to compare cpu and human choice and
    log wins or losses

--- script.js
+++ script.js
@@ -6,30 +6,67 @@ function getComputerChoice() {
     
     computerChoice = Math.random() * 3;
     if (computerChoice >= 0 && computerChoice < 1) {
-        return "Rock " + String(computerChoice);
+        return "rock";
     }
     else if (computerChoice >= 1 && computerChoice < 2) {
-        return "Paper " + String(computerChoice);
+        return "paper";
     }
-
     else {
-        return "Scissors " + String(computerChoice)
+        return "scissors";
     }
 }
-
-for (let i = 0; i < 15; i ++) {
-    console.log(getComputerChoice());
-}
-
 // function to get human choice
 
 function getHumanChoice() {
     humanChoice = prompt("Choose between Rock, Paper or Scissors");
+    console.log(humanChoice);
     return humanChoice;
 }
-console.log(getHumanChoice());
-
 // Declaring and Initializing human score and computer score
 
 humanScore = 0;
 computerScore = 0;
+
+// function to tell if computer or human won the round
+
+function playRound(humanChoice, computerChoice) {
+    
+    humanSelection = humanChoice.toLowerCase();
+    computerChoice = computerChoice;
+
+    if (humanSelection == "rock") {
+        if (computerChoice == "rock") {
+            console.log(`You drew with computer. ${humanSelection} ties with ${computerChoice}`);
+        }
+        else if (computerChoice == "paper") {
+            console.log(`You lost. ${humanSelection} loses against ${computerChoice}`);
+        }
+        else {
+            console.log(`You won. ${humanSelection} wins against ${computerChoice}`);
+        }
+    }
+    else if (humanSelection == "paper") {
+        if (computerChoice == "rock") {
+            console.log(`You won with computer. ${humanSelection} wins against ${computerChoice}`);
+        }
+        else if (computerChoice == "paper") {
+            console.log(`You drew. ${humanSelection} draws against ${computerChoice}`);
+        }
+        else {
+            console.log(`You lost. ${humanSelection} loses against ${computerChoice}`);
+        }
+    }
+    else {
+        if (computerChoice == "rock") {
+            console.log(`You lost against computer. ${humanSelection} loses against ${computerChoice}`);
+        }
+        else if (computerChoice == "paper") {
+            console.log(`You won. ${humanSelection} wins against ${computerChoice}`);
+        }
+        else {
+            console.log(`You drew. ${humanSelection} draws against ${computerChoice}`);
+        }
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
