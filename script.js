2038b6d7866fb9b8dac91fb3101abe6b6322ff95
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-05-08 11:36:24 +0545

    Create function called getComputerChoice
    Made function to return rock, paper or scissors
    randomly for cpu

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+console.log("Start of the program")
+
+// function to return rock, paper or scissors
+
+function getComputerChoice() {
+    
+    computerChoice = Math.random() * 3;
+    if (computerChoice >= 0 && computerChoice < 1) {
+        return "Rock" + String(computerChoice);
+    }
+    else if (computerChoice >= 1 && computerChoice < 2) {
+        return "Paper" + String(computerChoice);
+    }
+
+    else {
+        return "Scissors" + String(computerChoice)
+    }
+}
+
+for (let i = 0; i < 15; i ++) {
+    console.log(getComputerChoice());
+}
