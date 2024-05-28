e34d1b5033ec10802d2456b7bd25d781979948e6
Aashish Neupane <165367795+aashish-neu@users.noreply.github.com>
2024-05-28 17:03:21 +0545

    Make UI for rock, paper, scissors
    Made simple ui for rock, paper or scissors

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ console.log("Start of the program")
 let humanScore = 0;
 let computerScore = 0;
 
+// to display and update current human score and current computer score
+
+const displayHumanScore = document.querySelector(".current-human-score");
+const displayComputerScore = document.querySelector(".current-computer-score");
+
+displayHumanScore.innerText = "Not Won Yet. Better luck next time!!!";
+displayComputerScore.innerText = "Not Won Yet. Computer not as smart as you!!!";
+
 // buttons to playround with respective selection of rock, paper or scissors
 
 const rockButton = document.querySelector("#btnRock");
@@ -17,9 +25,17 @@ const scissorsButton = document.querySelector("#btnScissors");
 
 const resultSection = document.querySelector(".resultSection");
 const currentResult = document.querySelector(".currentResult");
+
+currentResult.innerText = "Finding Results........";
+
+// to display final outcome after 5 rounds
+
+const finalOutcomeConatiner = document.querySelector(".final-outcome-container");
+
 // resultSection.textContent = "Let's see who won!!!";
 // const output = document.querySelector(".output");
 // output.textContent = "needed to be added";
+
 
 // to playround when button is pressed
 
@@ -68,52 +84,113 @@ function playRound(humanChoice, computerChoice) {
     // changing console.logs to DOM methods
     computerChoice = getComputerChoice();
 
-    if (humanChoice == "rock") {
-        if (computerChoice == "rock") {
-            // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
-            currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
+
+    // to find out winner after player or cpu gets 5 wins
+
+    if (computerScore < 5 && humanScore < 5) {
+
+        if (humanChoice == "rock") {
+            if (computerChoice == "rock") {
+                // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
+                currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
+            }
+            else if (computerChoice == "paper") {
+                // console.log(`You lost. ${humanChoice} loses against ${computerChoice}`);
+                currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`;
+                computerScore += 1;
+                // to display running score
+                displayComputerScore.innerText = `Computer Score : ${computerScore}`;
+            }
+            else {
+                // console.log(`You won. ${humanChoice} wins against ${computerChoice}`);
+                currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
+                humanScore += 1;
+                // update
+                displayHumanScore.innerText = ` Human Score : ${humanScore}`;
+            }
         }
-        else if (computerChoice == "paper") {
-            // console.log(`You lost. ${humanChoice} loses against ${computerChoice}`);
-            currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`;
-            computerScore += 1;
+        else if (humanChoice == "paper") {
+            if (computerChoice == "rock") {
+                // console.log(`You won with computer. ${humanChoice} wins against ${computerChoice}`);
+                currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
+                humanScore += 1;
+                // update
+                displayHumanScore.innerText = ` Human Score : ${humanScore}`;
+                
+            }
+            else if (computerChoice == "paper") {
+                // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
+                currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
+            }
+            else {
+                // console.log(`You lost. ${humanChoice} loses against ${computerChoice}`);
+                currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`;
+                computerScore += 1;
+                // update
+                displayComputerScore.innerText = `Computer Score : ${computerScore}`;
+            }
         }
         else {
-            // console.log(`You won. ${humanChoice} wins against ${computerChoice}`);
-            currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
-            humanScore += 1;
+            if (computerChoice == "rock") {
+                // console.log(`You lost against computer. ${humanChoice} loses against ${computerChoice}`);
+                currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`
+                computerScore += 1;
+                // update
+                displayComputerScore.innerText = `Computer Score : ${computerScore}`;
+            }
+            else if (computerChoice == "paper") {
+                // console.log(`You won. ${humanChoice} wins against ${computerChoice}`);
+                currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
+                humanScore += 1;
+                // update
+                displayHumanScore.innerText = ` Human Score : ${humanScore}`;
+            }
+            else {
+                // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
+                currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
+            }
         }
-    }
-    else if (humanChoice == "paper") {
-        if (computerChoice == "rock") {
-            // console.log(`You won with computer. ${humanChoice} wins against ${computerChoice}`);
-            currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
-            humanScore += 1;
-        }
-        else if (computerChoice == "paper") {
-            // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
-            currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
-        }
-        else {
-            // console.log(`You lost. ${humanChoice} loses against ${computerChoice}`);
-            currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`;
-            computerScore += 1;
-        }
-    }
+    } 
+    else if (humanScore === 5) {
+        const finalOutcomeHeader = document.createElement("h3");
+        finalOutcomeHeader.classList.add = "header3";
+        finalOutcomeHeader.innerText = "Here is the final result :";
+        finalOutcomeConatiner.appendChild(finalOutcomeHeader);
+
+        const finalOutcome = document.createElement("p");
+        finalOutcome.innerText = "Congratulations. You won against the computer!!!";
+        finalOutcomeConatiner.appendChild(finalOutcome);
+
+        // to make button unresponsive
+        rockButton.innerText = "Please Don't Press Me Again";
+        paperButton.innerText = "Please Don't Press Me Again";
+        scissorsButton.innerText = "Please Don't Press Me Again";
+        rockButton.setAttribute("disabled", "true");
+        paperButton.setAttribute("disabled", "true");
+        scissorsButton.setAttribute("disabled", "true");
+    } 
+    
+    else if (computerScore === 5) {
+        const finalOutcomeHeader = document.createElement("h3");
+        finalOutcomeHeader.classList.add = "header3";
+        finalOutcomeHeader.innerText = "Here is the final result :";
+        finalOutcomeConatiner.appendChild(finalOutcomeHeader);
+
+        const finalOutcome = document.createElement("p");
+        finalOutcome.innerText = "Computer go brrrrrrrr. Better luck next time!!!";
+        finalOutcomeHeader.appendChild(finalOutcome);
+
+        // to make button unresponsive
+        rockButton.innerText = "Please Don't Press Me Again";
+        paperButton.innerText = "Please Don't Press Me Again";
+        scissorsButton.innerText = "Please Don't Press Me Again";
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.setAttribute("disabled", "true");
+    } 
     else {
-        if (computerChoice == "rock") {
-            // console.log(`You lost against computer. ${humanChoice} loses against ${computerChoice}`);
-            currentResult.textContent = `You lost. ${humanChoice} loses against ${computerChoice}`
-            computerScore += 1;
-        }
-        else if (computerChoice == "paper") {
-            // console.log(`You won. ${humanChoice} wins against ${computerChoice}`);
-            currentResult.textContent = `You won. ${humanChoice} wins against ${computerChoice}`;
-            humanScore += 1;
-        }
-        else {
-            // console.log(`You drew. ${humanChoice} draws against ${computerChoice}`);
-            currentResult.textContent = `You drew. ${humanChoice} draws against ${computerChoice}`;
-        }
+        finalOutcome.innerText = "Error Error";
     }
+
+    
 }
